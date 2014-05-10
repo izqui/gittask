6120c014dbc1753bd86c2b18e385a0aecc77ab2a
Jorge Izquierdo <jorge@izqui.me>
2014-05-10 11:37:49 +0200

    hey

--- conf.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const (
-	GithubClientId     = "4f5e5cc1e6a84983c672"
-	GithubClientSecret = "d82b3d4cc7cd8b50aaf1e19d9dc4c8e4d11f68a5"
-
-	GittaskProtocol = "http"
-	GittaskHost     = "localhost"
-)
